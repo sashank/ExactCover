d02516b6d2d4482199f577fd674713818b6cfae3
Sashank Dara <sadara@cisco.com>
2014-12-18 17:11:32 +0530

    it works . only caveat box constraint is hard coded

--- src/ExactCover.java
+++ src/ExactCover.java
@@ -38,13 +38,13 @@ public class ExactCover extends DLXSolver {
             nodeSmatrix[0][c] = new Node("C" + c, 4, null);
         }
 
-        int ec_cnt = 0, box_no = 1, box_r = 0, box_c =0;
-        ArrayList<String> exactcover_tagList = new ArrayList<String>(rows);
+        int ec_cnt = 0, box_no=0 ;
+        HashMap<String, String> exactCoverMatrix = new HashMap<String, String>(cols);
+        String key, value;
         // Outer three loops are Rows of the Exact Cover Matrix
         for(int su_row = 0 ; su_row < grid; su_row ++){
            for(int su_col = 0 ; su_col < grid; su_col ++) {
                 for (int su_val = 0; su_val < grid ; su_val++){
-
 
                     Boolean[] array = new Boolean[cols];
                     Arrays.fill(array, Boolean.FALSE);
@@ -54,79 +54,68 @@ public class ExactCover extends DLXSolver {
                     array[constraint_indx + ((su_row * grid ) + su_col)] = Boolean.TRUE;
 
                     // Row Constraint 81 - 161 Cols
-                    constraint_indx = constraint_indx + (grid * grid) -1;
+                    constraint_indx = constraint_indx + (grid * grid);
                     array[constraint_indx + ((su_row * grid ) + su_val)] = Boolean.TRUE;
 
                     // Col Constraint 162- 243 Cols
-                    constraint_indx = constraint_indx + (grid * grid) -1;
+                    constraint_indx = constraint_indx + (grid * grid);
                     array[constraint_indx + ((su_col * grid ) + su_val)] = Boolean.TRUE;
 
                     //  Box Constraint 244 - 323 Cols
-                    constraint_indx = constraint_indx + (grid * grid) -1;
+                    constraint_indx = constraint_indx + (grid * grid);
 
                     if( (su_row >=0 && su_row < 3) && (su_col >=0 && su_col < 3 ))
                         box_no = 0;
-                    if( (su_row >=0 && su_row < 3) && (su_col >=4 && su_col < 6 ))
+                    if( (su_row >=0 && su_row < 3) && (su_col >=3 && su_col < 6 ))
                         box_no = 1;
-                    if( (su_row >=0 && su_row < 3) && (su_col >=7 && su_col < 9 ))
+                    if( (su_row >=0 && su_row < 3) && (su_col >=6 && su_col < 9 ))
                         box_no = 2;
-                    if( (su_row >=4 && su_row < 6) && (su_col >=0 && su_col < 3 ))
+                    if( (su_row >=3 && su_row < 6) && (su_col >=0 && su_col < 3 ))
                         box_no = 3;
-                    if( (su_row >=4 && su_row < 6) && (su_col >=4 && su_col < 6 ))
+                    if( (su_row >=3 && su_row < 6) && (su_col >=3 && su_col < 6 ))
                         box_no = 4;
-                    if( (su_row >=4 && su_row < 6) && (su_col >=7 && su_col < 9 ))
+                    if( (su_row >=3 && su_row < 6) && (su_col >=6 && su_col < 9 ))
                         box_no = 5;
-                    if( (su_row >=7 && su_row < 9) && (su_col >=0 && su_col < 3 ))
+                    if( (su_row >=6 && su_row < 9) && (su_col >=0 && su_col < 3 ))
                         box_no = 6;
-                    if( (su_row >=7 && su_row < 9) && (su_col >=4 && su_col < 6 ))
+                    if( (su_row >=6 && su_row < 9) && (su_col >=3 && su_col < 6 ))
                         box_no = 7;
-                    if( (su_row >=7 && su_row < 9) && (su_col >=7 && su_col < 9 ))
+                    if( (su_row >=6 && su_row < 9) && (su_col >=6 && su_col < 9 ))
                         box_no = 8;
 
                     array[constraint_indx + (box_no * grid) + su_val] = Boolean.TRUE;
 
-                    System.out.print("r"+(su_row+1)+"c"+(su_col+1)+"#"+(su_val+1) +" : ");
-                    for(int j=0 ; j <cols; j++) {
-                      if(array[j] == Boolean.TRUE)
-                          System.out.print(1);
-                      else
-                          System.out.print(0);
-                    }
+                    key =  "r"+(su_row+1)+"c"+(su_col+1)+"#"+(su_val+1);
 
-                    System.out.println();
+                    StringBuilder builder = new StringBuilder();
+                    for (boolean bool: array) {
+                        if (bool) builder.append("1"); else builder.append("0");
+                    }
+                    value = builder.toString();
+                    exactCoverMatrix.put(key,value) ;
+                  //  System.out.println((constraint_indx + (box_no * grid) + su_val ) + " box " + box_no + "key " +key);
+                  //  System.out.println(key +" "+ value);
                     ec_cnt++;
                 }
              }
          }
 
-        Scanner sc = null;
-        try {
-            sc = new Scanner(new File("sudoku_matrix"));
-            // initialize data nodes in nodeSmatrix from file
-            int row = 1;
-            while(sc.hasNext()) {
-                String[] line = sc.nextLine().split(" ");
-                if(line[0].startsWith("#"))
-                    continue;
 
-                String tag = line[0];
-                String bits = line[1];
-
-                int col = 0;
-                for(char c : bits.toCharArray()) {
-                    if(c == '1') {
-                        nodeSmatrix[row][col] = new Node(tag, -1, nodeSmatrix[0][col]);
-                    // store first node in each row so we can remove the initial positions later
-                        if(rowtag2node.get(tag) == null)
-                            rowtag2node.put(tag, nodeSmatrix[row][col]);
-                    }
-                    ++col;
-                }
-                ++row;
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        int row = 1;
+         for(String tag : exactCoverMatrix.keySet()){
+             String bits = exactCoverMatrix.get(tag);
+             int col = 0;
+             for(char c : bits.toCharArray()) {
+                 if(c == '1') {
+                     nodeSmatrix[row][col] = new Node(tag, -1, nodeSmatrix[0][col]);
+                     // store first node in each row so we can remove the initial positions later
+                     if(rowtag2node.get(tag) == null)
+                         rowtag2node.put(tag, nodeSmatrix[row][col]);
+                 }
+                 ++col;
+             }
+             ++row;
+         }
 
         // initialize root
         Node root = new Node("root", -1, null);
